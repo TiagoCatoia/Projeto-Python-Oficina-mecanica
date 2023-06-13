86fc707ffcfcf3716d0dbb7d979fa3d89db70972
TiagoCatoia <133229612+TiagoCatoia@users.noreply.github.com>
2023-06-13 17:26:58 -0300

    Update and rename cadastrotrabalho.py to oficinamecanica.py

--- oficinamecanica.py
+++ oficinamecanica.py
@@ -66,6 +66,8 @@ def submenu_mecanicos():
         elif opcao == 4:
             print("Imprimindo todos...")
             imprimir_mecanicos(mecanicos)
+        else:
+            print("\nOpção inválida,tente novamente!")
     if opcao == 0:
         gravar_mecanico(arquivo_mecanicos,mecanicos)
         print("Voltando ao menu...")
@@ -97,6 +99,8 @@ def submenu_veiculos():
         elif opcao == 4:
             print("Imprimindo todos")
             imprimir_veiculos(veiculos)
+        else:
+            print("\nOpção inválida,tente novamente!")
     if opcao == 0:
         gravar_veiculos(arquivo_veiculos,veiculos)
         print("Voltando ao menu...")
@@ -128,8 +132,46 @@ def submenu_consertos():
         elif opcao == 4:
             print("Imprimindo todos...")
             imprimir_consertos(consertos)
+        else:
+            print("\nOpção inválida,tente novamente!")
     if opcao == 0:
         gravar_consertos(arquivo_consertos,consertos)
+        print("Voltando ao menu...")
+
+def submenu_relatorios():              ## COLOCAR UMA DESCRIÇÂO DO RELATORIO NA ESCOLHA DA OPCAO
+    opcao = 10
+    while opcao != 0:
+        print("Escolha uma opção ou 0 para voltar ao menu principal...")
+        print("1. Acessar o primeiro relatório")
+        print("2. Acessar o segundo relatório")
+        print("3. Acessar o terceiro relatório")
+        print("0. Voltar ao menu principal")
+        opcao = int(input("Digite sua opção: "))
+        if opcao == 1:
+            arquivo_mecanicos = "./dados_mecanicos.txt"
+            mecanicos = carregar_arq_mecanico(arquivo_mecanicos)
+            print("Abrindo o primeiro relatório...")
+            relatorio_1(mecanicos)
+        
+        elif opcao == 2:
+            arquivo_veiculos = "./dados_veiculos.txt"
+            veiculos = carregar_arq_veiculos(arquivo_veiculos)
+            print("Abrindo o segundo relatório...")
+            relatorio_2(veiculos)
+        
+        elif opcao == 3:
+            print("Abrindo o terceiro relatório...")
+            arquivo_mecanicos = "./dados_mecanicos.txt"
+            arquivo_veiculos = "./dados_veiculos.txt"
+            arquivo_consertos = "./dados_consertos.txt"
+            mecanicos = carregar_arq_mecanico(arquivo_mecanicos)
+            veiculos = carregar_arq_veiculos(arquivo_veiculos)
+            consertos = carregar_arq_consertos(arquivo_consertos)
+            relatorio_3(mecanicos,veiculos,consertos)
+        else:
+            print("\nOpção inválida,tente novamente!")
+
+    if opcao == 0:
         print("Voltando ao menu...")
 
 #########################################################################
@@ -154,7 +196,7 @@ def cadastrar_mecanicos(lista_mecanicos):                #CADASTRAR APENAS 1 ema
     achou = verificar_mecanico(lista_mecanicos, mecanico.cpf)
     if achou == -1:
         mecanico.nome = input("Digite o nome e sobrenome: ")
-        mecanico.data_nascimento = input("Digite a data de nascimento: ")
+        mecanico.data_nascimento = input("Digite a data de nascimento no formato (dd/mm/aaaa): ")
         mecanico.sexo = input("Digite o sexo: ")
         mecanico.salario = input("Digite o salário: ")
         email = input("Digite o email: ")
@@ -417,10 +459,10 @@ def cadastrar_consertos(lista_consertos):
     conserto = Conserto()
     conserto.placa_conserto = input("Digite a placa veículo do conserto: ")
     conserto.cpf_conserto = input("Digite o CPF: ")
-    conserto.data_entrada = input("Digite a data de entrada: ")
+    conserto.data_entrada = input("Digite a data de entrada no formato (dd/mm/aaaa): ")
     achou = verificar_conserto(lista_consertos, conserto.cpf_conserto, conserto.placa_conserto, conserto.data_entrada)
     if achou == -1:
-        conserto.data_saida = input("Digite a data de saída: ")
+        conserto.data_saida = input("Digite a data de saída no formato (dd/mm/aaaa): ")
         conserto.descricao_problemas = input("Descreva os problemas que levaram ao conserto: ")
         conserto.valor_conserto = input("Digite o valor do conserto: ")
     elif achou != -1:
@@ -432,7 +474,7 @@ def cadastrar_consertos(lista_consertos):
 def alterar_excluir_consertos(lista_consertos):
     placa_conserto = input("Informe a placa do conserto: ")
     cpf_conserto = input("Digite o CPF: ")
-    data_entrada = input("Digite a data de entrada: ")
+    data_entrada = input("Digite a data de entrada no formato (dd/mm/aaaa): ")
     posicao = verificar_conserto(lista_consertos, cpf_conserto, placa_conserto, data_entrada )
     if posicao != -1:
         print("Se deseja alterar um dado digite 1")
@@ -447,7 +489,7 @@ def alterar_excluir_consertos(lista_consertos):
             print("3 - Valor do conserto")
             escolha = int(input("Digite a opção que deseja alterar: "))
             if escolha == 1:
-                lista_consertos[posicao].data_saida = input("Digite a nova data de saída: ")
+                lista_consertos[posicao].data_saida = input("Digite a data de saída no formato (dd/mm/aaaa): ")
             elif escolha == 2:
                 lista_consertos[posicao].descricao_problemas = input("Digite a nova descrição dos problemas: ")
                 print("Descrição alterada")
@@ -486,7 +528,7 @@ def imprimir_consertos(lista_consertos):
 def imprimir_um_conserto(lista_consertos):
     placa_conserto = input("Informe a placa do conserto: ")
     cpf_conserto = input("Digite o CPF: ")
-    data_entrada = input("Digite a data de entrada: ")
+    data_entrada = input("Digite a data de entrada no formato (dd/mm/aaaa): ")
     i = verificar_conserto(lista_consertos, cpf_conserto, placa_conserto, data_entrada )
     if i != -1:
         print('CPF Conserto: ' + lista_consertos[i].cpf_conserto + " | " + 'Placa Conserto: ' + lista_consertos[i].placa_conserto + ' | ' + "Data de entrada: " + lista_consertos[i].data_entrada + " | " + "Data de Saída: " + lista_consertos[i].data_saida + " | " + "Descrição dos problemas: " + lista_consertos[i].descricao_problemas + " | " + "Valor do conserto: " + lista_consertos[i].valor_conserto)
@@ -519,6 +561,67 @@ def carregar_arq_consertos(arquivo_consertos):
             dados_consertos.append(cons)
         arq.close()
     return dados_consertos
+##########################################################################
+
+def relatorio_1(lista_mecanicos):
+    achou = False
+    idade_limite = int(input("Digite a idade limite:"))
+    for i in range(len(lista_mecanicos)):
+        dat_nasc = lista_mecanicos[i].data_nascimento
+        dat_nasc = dat_nasc.split("/")
+        dia = int(dat_nasc[0])
+        mes = int(dat_nasc[1])
+        ano = int(dat_nasc[2])
+        from datetime import date
+        hoje = date.today()
+        idade_mecanico = hoje.year - ano
+        if (hoje.month, hoje.day) < (mes, dia):
+            idade_mecanico -= 1
+        if idade_mecanico > idade_limite:
+            achou = True
+            print(f"CPF: {lista_mecanicos[i].cpf}  |  Nome: {lista_mecanicos[i].nome}  |  Data de nascimento: {lista_mecanicos[i].data_nascimento}  |  Sexo: {lista_mecanicos[i].sexo}  |  Salário: {lista_mecanicos[i].salario}  |  E-mail(s): {lista_mecanicos[i].email}  |  Telefone(s): {lista_mecanicos[i].telefone}")
+    if achou == False:
+        print("\nNão existem mecanicos acima desta idade limite cadastrados!")
+        
+def relatorio_2(lista_veiculos):
+    achou = False
+    marca = input("Digite a marca do carro:")
+    for i in range(len(lista_veiculos)):
+        if lista_veiculos[i].marca == marca:
+            achou = True
+            print('Placa: ' + lista_veiculos[i].placa + " | " + 'Tipo: ' + lista_veiculos[i].tipo + ' | ' + "Marca: " + lista_veiculos[i].marca + " | " + "Modelo: " + lista_veiculos[i].modelo + " | " + "Ano: "+  lista_veiculos[i].ano + " | " + "Portas: " + lista_veiculos[i].portas + " | " + "Combustível: " + lista_veiculos[i].combustivel + " | " + "Cor: " + lista_veiculos[i].cor)
+    if achou == False:
+        print("\nNão existem carros desta marca cadastrados!")
+    
+def relatorio_3(lista_mecanicos,lista_veiculos,lista_consertos):
+    achou = False
+    data_inicio_str = input("Digite a data de início no formato (dd/mm/aaaa): ")
+    data_fim_str = input("Digite a data de fim no formato (dd/mm/aaaa): ")
+    for i in range(len(lista_consertos)):
+        data_verificar_str = lista_consertos[i].data_entrada
+        data_verificar_str = data_verificar_str.split("/")
+        dia = int(data_verificar_str[0])
+        mes = int(data_verificar_str[1])
+        ano = int(data_verificar_str[2])
+        data_verificar_str = str(dia) + "/" + str(mes) + "/" + str(ano)
+
+        from datetime import datetime
+        data_inicio = datetime.strptime(data_inicio_str, "%d/%m/%Y")
+        data_fim = datetime.strptime(data_fim_str, "%d/%m/%Y")
+        data_verificar = datetime.strptime(data_verificar_str, "%d/%m/%Y")
+
+        if data_inicio <= data_verificar <= data_fim:
+            achou = True
+            for t in range(len(lista_mecanicos)):
+                if lista_mecanicos[t].cpf == lista_consertos[i].cpf_conserto:
+                    print(f"CPF: {lista_mecanicos[t].cpf} | Nome: {lista_mecanicos[t].nome}",end=" ")
+            for t in range(len(lista_veiculos)):
+                if lista_veiculos[t].placa == lista_consertos[i].placa_conserto:
+                    print(f" | Placa: {lista_veiculos[t].placa} | Marca: {lista_veiculos[t].marca} | Modelo: {lista_veiculos[t].modelo} | Ano: {lista_veiculos[t].ano}",end=" ")
+            print(f" | Data de entrada para conserto: {lista_consertos[i].data_entrada} e Data de saída: {lista_consertos[i].data_saida} | Descrição do problema: {lista_consertos[i].descricao_problemas} | Valor do conserto: {lista_consertos[i].valor_conserto}")
+
+    if achou == False:
+        print("Não existem consertos cadastrados entre essas datas!")
 
 ##########################################################################
 
@@ -536,6 +639,7 @@ def main():
             submenu_consertos()
         elif opcao == 4:
             print("Abrindo Submenu de Relatórios...")
+            submenu_relatorios()
         else:
             print("Opção inválida!")
             print("Digite novamente...")
